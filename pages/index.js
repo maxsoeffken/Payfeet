cca489c65dfd9ba18a1d4d82c23e73fff3a2c9dc
maxsoeffken <maxsoeffken@icloud.com>
2025-08-23 22:20:11 +0200

    Update index.js
    Signed-off-by: maxsoeffken <maxsoeffken@icloud.com>

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,7 @@
-// /pages/index.js
-import Link from 'next/link';
-
-export default function Home() {
-  return (
-    <main style={{ maxWidth: 720, margin: '40px auto', padding: 16 }}>
-      <h1>Payfeet-App</h1>
-      <p>It works! 🎉</p>
-      <p>
-        <Link href="/login">Login</Link> &nbsp;|&nbsp; <Link href="/register">Register</Link>
-      </p>
-    </main>
-  );
+// pages/index.js
+export async function getServerSideProps() {
+  return {
+    redirect: { destination: '/login', permanent: false },
+  };
 }
+export default function Home() { return null; }
