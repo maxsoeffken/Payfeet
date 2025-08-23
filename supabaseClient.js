625eff2a5ce816b250ff0e681a778cf24bf215f9
maxsoeffken <maxsoeffken@icloud.com>
2025-08-23 15:22:07 +0200

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = "https://mqxmwtrldkcugetwpgtw.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xeG13dHJsZGtjdWdldHdwZ3R3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU4ODk5NzMsImV4cCI6MjA3MTQ2NTk3M30.sxcao5vKHYQIH61Wdi69mr5hVByAk2IjV_hjP0UOZhc"
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
