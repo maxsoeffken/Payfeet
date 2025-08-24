d753482dc321cabab9f821c55908f77fd21c339a
maxsoeffken <maxsoeffken@icloud.com>
2025-08-24 23:54:39 +0200

    Create notifications.js
    Signed-off-by: maxsoeffken <maxsoeffken@icloud.com>

---
+++ pages/notifications.js
@@ -0,0 +1,8 @@
+import Layout from "../components/Layout";
+export default function Notifications() {
+  return (
+    <Layout title="Benachrichtigungen">
+      <p>Benachrichtigungen (Demo).</p>
+    </Layout>
+  );
+}
