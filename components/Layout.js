f736f52b12169097a2736ce6dcb6b90735942ba9
maxsoeffken <maxsoeffken@icloud.com>
2025-08-24 09:52:37 +0200

    Update Layout.js
    Signed-off-by: maxsoeffken <maxsoeffken@icloud.com>

--- components/Layout.js
+++ components/Layout.js
@@ -1,1 +1,11 @@
+// components/Layout.js
+import TopNav from "./TopNav";
 
+export default function Layout({ children }) {
+  return (
+    <div className="min-h-screen bg-[#77AEEE]">
+      <TopNav />
+      <main className="mx-auto max-w-5xl px-4 py-6">{children}</main>
+    </div>
+  );
+}
