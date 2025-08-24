5a2b0576bc53dcf78a4fa025b2ceb19bba8811fb
maxsoeffken <maxsoeffken@icloud.com>
2025-08-24 21:16:31 +0200

    Update index.js
    Signed-off-by: maxsoeffken <maxsoeffken@icloud.com>

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
+// pages/index.js
 export async function getServerSideProps() {
   return {
-    redirect: { destination: "/feed", permanent: false },
+    redirect: { destination: '/feed', permanent: false },
   };
 }
 export default function Home() { return null; }
