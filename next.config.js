881852e78cb754bceb73b8220893ec3151e83400
maxsoeffken <maxsoeffken@icloud.com>
2025-08-25 21:25:04 +0200

    Update next.config.js
    Signed-off-by: maxsoeffken <maxsoeffken@icloud.com>

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      // Falls du später <Image/> nutzt; aktuell verwenden wir <img/>
+      { protocol: 'https', hostname: '**.supabase.co' }
+    ]
+  }
+};
 module.exports = nextConfig;
